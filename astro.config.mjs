7dc8e28d0dcf8ac191deced76cde9fd9047ecb3e
pekochan069 <pekochan069@gmail.com>
2024-03-05 02:12:02 +0900

    Add alpine and htmx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
+import htmx from "astro-htmx";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [tailwind()],
-  output: "server",
+  integrations: [tailwind(), htmx(), alpinejs()],
+  output: "server"
 });
