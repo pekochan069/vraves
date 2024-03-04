5dd7bb7099f52a1f44ed3640ae73cb631e793633
pekochan069 <pekochan069@gmail.com>
2024-03-05 02:50:50 +0900

    Remove astro alpine integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import htmx from "astro-htmx";
 
-import alpinejs from "@astrojs/alpinejs";
-
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [tailwind(), htmx(), alpinejs()],
-  output: "server"
+  integrations: [tailwind(), htmx()],
+  output: "server",
 });
