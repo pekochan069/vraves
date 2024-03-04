1b0ff9aeed4a5bc283a1d4e63fc672515be182b2
pekochan069 <pekochan069@gmail.com>
2024-03-04 19:57:55 +0900

    Switch back to tailwind, implement shadcn button

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,43 @@
 /* eslint-env node */
 module.exports = {
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended-type-checked",
-		"plugin:astro/recommended",
-		"plugin:perfectionist/recommended-natural",
-		"prettier",
-	],
-	plugins: ["@typescript-eslint", "perfectionist"],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		project: true,
-		tsconfigRootDir: __dirname,
-	},
-	root: true,
-	overrides: [
-		{
-			files: ["*.astro"],
-			parser: "astro-eslint-parser",
-			parserOptions: {
-				parser: "@typescript-eslint/parser",
-				extraFileExtensions: [".astro"],
-			},
-			processor: "astro/client-side-ts",
-			rules: {
-				// "astro/no-set-html-directive": "error"
-			},
-		},
-		// ...
-		{
-			files: ["*.css.ts"],
-			rules: {
-				"perfectionist/sort-objects": "off",
-			},
-		},
-	],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:astro/recommended",
+    "plugin:perfectionist/recommended-natural",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "perfectionist"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      processor: "astro/client-side-ts",
+      rules: {
+        // "astro/no-set-html-directive": "error"
+      },
+    },
+    {
+      files: ["src/components/ui/*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      processor: "astro/client-side-ts",
+      rules: {
+        "perfectionist/sort-objects": "off",
+      },
+    },
+  ],
 };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
+import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: vercel(),
-	integrations: [],
-	output: "server",
+  adapter: vercel(),
+  integrations: [tailwind()],
+  output: "server",
 });
