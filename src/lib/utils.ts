9a61a0aecd07d954efa54352f4528a39e0f534ae
pekochan069 <pekochan069@gmail.com>
2024-03-05 21:40:38 +0900

    Add comments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ *
+ * @param inputs class list to merge
+ * @returns merged classes
+ *
+ * @example
+ * ```tsx
+ * <div className={cn('text-red-500', 'bg-blue-500', 'p-4')} />
+ * ```
+ */
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
